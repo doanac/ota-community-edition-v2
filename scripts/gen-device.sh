cf6048385f77e0bf99a007db1078af9bd0c73cbb
Andy Doan <andy@foundries.io>
2024-01-16 20:34:41 -0600

    Have gen-device work better for a real server and aktualizr
    Signed-off-by: Andy Doan <andy@foundries.io>

--- scripts/gen-device.sh
+++ scripts/gen-device.sh
@@ -7,10 +7,14 @@ CWD=$(dirname "$0")
 SERVER_DIR=$CWD/../ota-ce-gen
 DEVICES_DIR=${SERVER_DIR}/devices
 
+GATEWAY=$(openssl x509 -in  $SERVER_DIR/server.crt -noout -ext subjectAltName  | grep DNS: | cut -d: -f2)
+
 device_id=$DEVICE_UUID
 device_dir="${DEVICES_DIR}/${DEVICE_UUID}"
 
 mkdir -p "${device_dir}"
+
+echo "Gateway URL is: $GATEWAY"
 
 openssl ecparam -genkey -name prime256v1 | openssl ec -out "${device_dir}/pkey.ec.pem"
 openssl pkcs8 -topk8 -nocrypt -in "${device_dir}/pkey.ec.pem" -out "${device_dir}/pkey.pem"
@@ -18,7 +22,7 @@ openssl pkcs8 -topk8 -nocrypt -in "${device_dir}/pkey.ec.pem" -out "${device_dir
 openssl req -new -key "${device_dir}/pkey.pem" \
           -config <(sed "s/\$ENV::DEVICE_UUID/${DEVICE_UUID}/g" "${CWD}/certs/client.cnf") \
           -out "${device_dir}/${device_id}.csr"
-  
+
 openssl x509 -req -days 365 -extfile "${CWD}/certs/client.ext" -in "${device_dir}/${device_id}.csr" \
         -CAkey "${DEVICES_DIR}/ca.key" -CA "${DEVICES_DIR}/ca.crt" -CAcreateserial -out "${device_dir}/client.pem"
 
@@ -36,30 +40,34 @@ body=$(cat <<END
 END
     )
 
-cat > ${device_dir}/config.toml <<EOF
+cat > ${device_dir}/sota.toml <<EOF
 [provision]
-primary_ecu_hardware_id = "ota-ce-device"
+primary_ecu_hardware_id = "intel-corei7-64"
 
 [tls]
-server_url_path = "gateway.url"
+server = "https://${GATEWAY}:30443"
 
 [logger]
-loglevel = 0
+loglevel = 1
 
 [storage]
-path = "storage"
+path = "/var/sota-uptane"
 
 [pacman]
-type = "none"
-images_path = "storage/images"
+type = "ostree"
+ostree_sever = "https://${GATEWAY}:30443/treehub"
+
+[uptane]
+key_source = "file"
+repo_server = "https://${GATEWAY}:30443/repo"
+director_server = "https://${GATEWAY}:30443/director"
+polling_sec = 30
 
 [import]
-base_path = "."
+base_path = "/var/sota-uptane"
 tls_cacert_path = "ca.pem"
 tls_clientcert_path = "client.pem"
 tls_pkey_path = "pkey.pem"
 EOF
 
-curl -X PUT -d "${body}" http://deviceregistry.ota.ce/api/v1/devices -s -S -v -H "Content-Type: application/json" -H "Accept: application/json, */*"
-
-echo "https://ota.ce:30443" > ${device_dir}/gateway.url
+curl -X PUT -d "${body}" http://deviceregistry.${GATEWAY}/api/v1/devices -s -S -v -H "Content-Type: application/json" -H "Accept: application/json, */*"
